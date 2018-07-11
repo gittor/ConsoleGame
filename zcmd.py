85dec34d066bb7fa59f83102ff3c139391ef69de
gittor <2317365219@qq.com>
2018-07-11 17:33:00 +0800

    zcmd

--- zcmd.py
+++ zcmd.py
@@ -1,4 +1,21 @@
 from monotonic import monotonic
+import os,sys
+
+def run(cmd):
+	code = os.system('%s > zcmd_run_180de67b5fcb.tmp 2>&1'%(cmd))
+	with open('zcmd_run_180de67b5fcb.tmp') as fin:
+		msg = fin.read()
+	os.remove('zcmd_run_180de67b5fcb.tmp')
+	return (code, msg)
+
+def where(fname):
+	candy = ['.']
+	candy.extend(sys.path)
+	for path in candy:
+		full = os.path.join(path, fname)
+		if os.path.isfile(full):
+			return full
+	return None
 
 def call_runtime(func, *keys, **args):
 	def _mono(*keys, **args):
